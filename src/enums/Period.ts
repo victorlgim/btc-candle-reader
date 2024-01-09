fed08e577192fc2bab3b2d469ab71fe2741dcaee
victorlgim <victor.guedes888@gmail.com>
2024-01-09 08:29:30 -0300

    feat: :sparkles: Created enum responsible for the candle update period

---
+++ src/enums/Period.ts
@@ -0,0 +1,12 @@
+enum Period {
+    TEN_SECONDS = 10000,
+    THIRD_SECONDS = 30000,
+    ONE_MINUTES = 60000,
+    FIVE_MINUTES = 300000,
+    TEN_MINUTES = 600000,
+    HALF_HOUR = 1800000,
+    HOUR = 3600000,
+    DAY = 86400000
+}
+
+export default Period
